fd5b74eafb7e2e3a57cce70fe4c4e575eaaae2fe
Erick Dae <20083000013@student.unmer.ac.id>
2023-02-21 16:19:56 +0700

    latihan java dasar membuat program todolist

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         methodTesting();
 
     }
+//    tampilkan semua elemen dalam array yang tidak null
     public static void showData(){
         for (var i=0; i<model.length; i++){
             var data = model[i];
@@ -17,6 +18,7 @@ public class Main {
         }
     }
     public static void addData(String data){
+//        cek kapasitas array
         var isFull = true;
         for (var i=0; i< model.length; i++){
             if (model[i] == null){
@@ -24,6 +26,7 @@ public class Main {
                 break;
             }
         }
+//        resize array
         if (isFull){
             var temp = model;
             model = new String[model.length * 2];
@@ -31,6 +34,7 @@ public class Main {
                 model[i] = temp[i];
             }
         }
+//        kalau elemennya null isi dengan data
         for (var i=0; i<model.length; i++){
             if (model[i] == null){
                 model[i] = data;
@@ -38,22 +42,28 @@ public class Main {
             }
         }
     }
+//    inputan untuk menerima data tipe data string
     public static String iptStr(String message){
         System.out.print("| >> "+message+" : ");
         return scanner.nextLine();
     }
+//    inputan untuk menerima tipe data integer
     public static int iptInt(String message){
         System.out.print("| >> "+message+" : ");
         int i = scanner.nextInt();
         return i;
     }
     public static boolean removeData(int number){
+//        kalau inputan - 1 lebih kecil dari 0 atau lebih besasr dari panjang array atau kosong maka kembalikan ke false
         if (((number - 1) >= model.length) || ((number - 1) < 0) || (model[number - 1] == null)){
             return false;
         }else{
+//            baca semua elemen mulai dari inputan number - 1 sampai panjang array
             for (var i=(number-1); i< model.length; i++){
+//                kalau i (number - 1) sama dengan panjang array - 1 maka kosongkan pilihannya
                 if (i == (model.length - 1)){
                     model[i] = null;
+//                    selain itu geser data yang tidak null keatas
                 }else{
                     model[i] = model[i+1];
                 }
